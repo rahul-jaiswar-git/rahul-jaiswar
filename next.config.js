a53858e5ce755e88f8f676fdd5104379ee6dda6f
Rahul Jaiswar <Rahuljaiswarofficial@gmail.com>
2025-01-19 23:17:10 +0530

    Secodn Update

---
+++ next.config.js
@@ -0,0 +1,17 @@
+const nextConfig = {
+  images: {
+    domains: ['hebbkx1anhila5yf.public.blob.vercel-storage.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  }
+}
+
+module.exports = nextConfig
+
